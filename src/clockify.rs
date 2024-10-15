b3cf55eca9559d4287d14c01157aa215d91f5283
Jape <jarpe2@hotmail.fi>
2024-10-15 20:07:16 +0300

    Fix debug JSON name

--- src/clockify.rs
+++ src/clockify.rs
@@ -417,7 +417,7 @@ impl ClockifyClient {
         }
 
         if get_settings().await.debug {
-            let path = format!("work_items_{}.json", Utc::now().format("%Y%m%d%H%M%S"));
+            let path = format!("time_off_items_{}.json", Utc::now().format("%Y%m%d%H%M%S"));
             if let Err(e) = json_to_disk(path, &time_off_items).await {
                 println!("Failed to time off items to disk! {e}")
             };
